0450aeb80de4b99f89fd59e6bd09c42a4afb2511
sanyamk23 <sanyamkumat2305@gmail.com>
2026-09-01 00:24:51 +0530

    Revert accidental changes to listener.py

--- providers/openlineage/src/airflow/providers/openlineage/plugins/listener.py
+++ providers/openlineage/src/airflow/providers/openlineage/plugins/listener.py
@@ -64,7 +64,7 @@ from airflow.utils.state import TaskInstanceState
 if TYPE_CHECKING:
     from sqlalchemy.orm import Session
 
-from airflow.sdk.execution_time.task_runner import RuntimeTaskInstance
+    from airflow.sdk.execution_time.task_runner import RuntimeTaskInstance
 
 if sys.platform == "darwin":
     from setproctitle import getproctitle
